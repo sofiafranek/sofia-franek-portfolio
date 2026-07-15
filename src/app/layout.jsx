58650fe24601ff6feca58757415f63de05eaf590
Sofia Franek <sofiafranek@gmail.com>
2026-07-15 16:04:59 +0100

    added a favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,22 +2,27 @@ import "./globals.css";
 import GlobalStyles from "@/styles/global";
 
 export const metadata = {
-  title: 'Sofia Franek',
-  description: 'Graphic Designer'
-}
+  title: "Sofia Franek - Graphic Designer",
+  description: "Graphic Designer",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
-<html lang="en" style={{ backgroundColor: "#f8efeb" }}>
-  <body
-    style={{
-      margin: 0,
-      backgroundColor: "#f8efeb"
-    }}
-  >
+    <html lang="en" style={{ backgroundColor: "#f8efeb" }}>
+      <body
+        style={{
+          margin: 0,
+          backgroundColor: "#f8efeb",
+        }}
+      >
         <GlobalStyles />
         {children}
       </body>
     </html>
-  )
+  );
 }
